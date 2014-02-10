d1dc14411b35d34b3268c6b343e62887de7cd859
so <katogiso.projectd@gmail.com>
2014-02-10 23:34:24 +0900

    1st Commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <QApplication>
+#include <QHeaderView>
+#include <QStandardItemModel>
+#include <QTableView>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    return app.exec();
+}
